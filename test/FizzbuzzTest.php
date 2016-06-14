dde4f3629539fa4732f9b6f0b7f02ef920b3518f
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-06-14 13:53:03 +0900

    fizzbuzzリファクタ

--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -5,40 +5,37 @@ require_once('FizzBuzz.php');
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-  public function test１５を入力したらfizzbuzzが帰ってくる()
-  {
-    $this->assertEquals('FizzBuzz',(new FizzBuzz(15))->start());
-  }
-  public function test３を入力したらfizzが帰ってくる()
-  {
-    $this->assertEquals('Fizz',(new FizzBuzz(3))->start());
-  }
-  public function test５を入力したらbuzzが帰ってくる()
-  {
-    $this->assertEquals('Buzz',(new FizzBuzz(5))->start());
-  }
-  public function test30を入力したらfizzbuzzが帰ってくる()
-  {
-    $this->assertEquals('FizzBuzz',(new FizzBuzz(30))->start());
-  }
-  public function test6を入力したらfizzが帰ってくる()
-  {
-    $this->assertEquals('Fizz',(new FizzBuzz(6))->start());
-  }
-  public function test10を入力したらbuzzが帰ってくる()
-  {
-    $this->assertEquals('Buzz',(new FizzBuzz(10))->start());
-  }
-  public function test1を入力したら1が帰ってくる()
-  {
-    $this->assertEquals('1',(new FizzBuzz(1))->start());
-  }
-  public function test2を入力したら2が帰ってくる()
-  {
-    $this->assertEquals('2',(new FizzBuzz(2))->start());
-  }
   public function test0を入力したら0が帰ってくる()
   {
     $this->assertEquals('0',(new FizzBuzz(0))->start());
   }
+
+  public function test３の倍数を入力したらfizzが帰ってくる()
+  {
+    $this->assertEquals('Fizz',(new FizzBuzz(3))->start(),'fail at 3');
+    $this->assertEquals('Fizz',(new FizzBuzz(6))->start(),'fail at 6');
+  }
+
+  
+  public function test５の倍数を入力したらbuzzが帰ってくる()
+  {
+    $this->assertEquals('Buzz',(new FizzBuzz(5))->start(),'fail at 5');
+    $this->assertEquals('Buzz',(new FizzBuzz(10))->start(),'fail at 10');
+  }
+
+
+  public function test１５を入力したらfizzbuzzが帰ってくる()
+  {
+    $this->assertEquals('FizzBuzz',(new FizzBuzz(15))->start(),'fail at 15');
+    $this->assertEquals('FizzBuzz',(new FizzBuzz(30))->start(),'fail at 30');
+  }
+
+
+  public function testそれ以外を入力したら1が帰ってくる()
+  {
+    $this->assertEquals('1',(new FizzBuzz(1))->start(),'fail at 1');
+    $this->assertEquals('2',(new FizzBuzz(2))->start(),'fail at 2');
+  }
+
+
 }
