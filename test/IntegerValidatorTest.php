871bd07da0ee397de48c8c0448bcbd994c2c0860
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-06-28 14:55:46 +0900

    itegervalidatorの整数以外の文字列のときのテスト

--- test/IntegerValidatorTest.php
+++ test/IntegerValidatorTest.php
@@ -6,4 +6,9 @@ class IntegerValidatorTest extends PHPUnit_Framework_TestCase
     {
         $this->assertTrue((new IntegerValidator('1'))->isInteger());
     }
+
+    public function test整数以外の文字列が選ばれたらfalseを返す()
+    {
+        $this->assertFalse((new IntegerValidator('a'))->isInteger());
+    }
 }
