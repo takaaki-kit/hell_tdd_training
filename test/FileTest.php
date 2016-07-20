50e4116c02c321e78596d495e6a118bf5af737a7
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-07-20 15:44:51 +0900

    setUpもファイル削除の処理をオーバーライド

--- test/FileTest.php
+++ test/FileTest.php
@@ -7,6 +7,11 @@ class FileTest extends PHPUnit_Framework_TestCase
 {
     const FILE_PATH = 'logs/answer.txt';
 
+    protected function setUp()
+    {
+        $this->delete_file();
+    }
+
     public function testファイルの中身をすべて返す()
     {
         $file = new File(self::FILE_PATH); 
@@ -17,6 +22,11 @@ class FileTest extends PHPUnit_Framework_TestCase
     }
 
     protected function tearDown()
+    {
+        $this->delete_file();
+    }
+
+    private function delete_file()
     {
         if(file_exists(self::FILE_PATH)){
             unlink(self::FILE_PATH);
